897c9477acd27d3c9200d095786c360c198afa90
kzkaktus <thekzkaktus@gmail.com>
2022-02-08 06:07:47 +0100

    relations handling

--- database_init.py
+++ database_init.py
@@ -1,9 +1,14 @@
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://0.0.0.0:27017')
-db=client["users_database"]
-col=db["users"]
-col.insert_one({"username": "kaktus", "password": "kaktus"})
-col.insert_one({"username": "kamil", "password": "kamil"})
+# client = MongoClient('mongodb://0.0.0.0:27017')
+# db=client["users_database"]
+# col=db["users"]
+# col.insert_one({"username": "kaktus", "password": "kaktus"})
+# col.insert_one({"username": "kamil", "password": "kamil"})
+
+client = MongoClient('mongodb://0.0.0.0:27018')
+db=client["relations_database"]
+col=db["invitations"]
+col.insert_one({"from": "janpaweldrugi", "to": "kaktus"})
 
 
--- front/app.py
+++ front/app.py
@@ -10,6 +10,11 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 's3cret'
 
+def get_username(request):
+    token = request.cookies.get('auth_token')
+    data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
+    return data["user"]
+
 def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
@@ -41,6 +46,53 @@ def token_prohibited(f):
 @token_required
 def root():
     return render_template('main.html')
+
+@app.route('/friends')
+@token_required
+def friends():
+    response = requests.post(
+            'http://relations-node:2138/friendscontent',
+            json={"username": get_username(request)})
+    friends=response.json()["friends"]
+    invites=response.json()["invites"]
+    return render_template('friends.html', friends=friends, invites=invites)
+
+@app.route('/api/accept', methods=['POST'])
+@token_required
+def api_accept():
+    print('Here at least', file=sys.stderr)
+    print('at:', request.json["auth_token"], file=sys.stderr)
+    response = requests.post(
+            'http://relations-node:2138/accept',
+            json=request.json
+    )
+    return make_response("OK", 200)
+
+@app.route('/api/decline', methods=['POST'])
+@token_required
+def api_decline():
+    requests.post(
+            'http://relations-node:2138/decline',
+            json=request.json)
+    return make_response("OK", 200)
+
+
+@app.route('/api/unfriend', methods=['POST'])
+@token_required
+def api_unfriend():
+    requests.post(
+            'http://relations-node:2138/unfriend',
+            json=request.json)
+    return make_response("OK", 200)
+
+@app.route('/api/invite', methods=['POST'])
+@token_required
+def api_invite():
+    requests.post(
+            'http://relations-node:2138/invite',
+            json=request.json)
+    return make_response("OK", 200)
+
 
 
 @app.route('/login', methods=['POST', 'GET'])
@@ -78,6 +130,12 @@ def register():
         return render_template('register.html', succ_info="Account created")
     else:
         return render_template('register.html', fail_info="Username not available")
+
+
+@app.route('/logout', methods=['POST'])
+@token_required
+def logout():
+    return make_response("OK", 200)
 
 
 if __name__ == '__main__':
